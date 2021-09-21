53fa74a8277af55ff2783bcb80c91e3a673f1ff2
P3trur0 <ubaldopescatore@gmail.com>
2021-09-21 11:26:40 +0200

    typo fixed

--- src/main/scala/it/pagopa/pdnd/interop/uservice/catalogprocess/errors/DescriptorNotFound.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/catalogprocess/errors/DescriptorNotFound.scala
@@ -3,4 +3,4 @@ package it.pagopa.pdnd.interop.uservice.catalogprocess.errors
 /** ADT modeling a missing descriptor error
   * @param message
   */
-final case class DescriptorNotFound(message: String) extends Throwable(s"Descriptor not found :: $message")
+final case class DescriptorNotFound(message: String) extends Throwable(s"Descriptor not found: $message")
