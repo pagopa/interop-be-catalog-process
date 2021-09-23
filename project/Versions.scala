4d06d93f3dac59b5a9e42274bd184563b14d777a
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2021-09-23 13:49:36 +0200

    Pin 549: remove unused awsSdkVersion

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,6 @@
 object Versions {
   lazy val akkaVersion                        = "2.6.15"
   lazy val akkaHttpVersion                    = "10.2.5"
-  lazy val awsSdkVersion                      = "2.4.17"
   lazy val projectionVersion                  = "1.2.1"
   lazy val cassandraPersistenceVersion        = "1.0.5"
   lazy val jdbcPersistenceVersion             = "5.0.1"
