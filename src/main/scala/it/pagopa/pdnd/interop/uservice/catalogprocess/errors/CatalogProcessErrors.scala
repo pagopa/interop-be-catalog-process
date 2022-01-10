c5d2ef131500073307decbd6c5264456079f90a9
P3trur0 <ubaldopescatore@gmail.com>
2022-01-10 09:51:23 +0100

    error codes fixed

--- src/main/scala/it/pagopa/pdnd/interop/uservice/catalogprocess/errors/CatalogProcessErrors.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/catalogprocess/errors/CatalogProcessErrors.scala
@@ -51,22 +51,6 @@ object CatalogProcessErrors {
   final case class CreateDescriptorDocumentError(descriptorId: String, eServiceId: String)
       extends ComponentError("0017", s"Error while creating document for $descriptorId for E-Service $eServiceId")
 
-  final case class GetDescriptorDocumentBadRequest(documentId: String, descriptorId: String, eServiceId: String)
-      extends ComponentError(
-        "0015",
-        s"Error retrieving document $documentId for E-Service $eServiceId and descriptor $descriptorId"
-      )
-  final case class GetDescriptorDocumentNotFound(documentId: String, descriptorId: String, eServiceId: String)
-      extends ComponentError(
-        "0016",
-        s"Error retrieving document $documentId for E-Service $eServiceId and descriptor $descriptorId"
-      )
-  final case class GetDescriptorDocumentError(documentId: String, descriptorId: String, eServiceId: String)
-      extends ComponentError(
-        "0017",
-        s"Error retrieving document $documentId for E-Service $eServiceId and descriptor $descriptorId"
-      )
-
   final case object FlattenedEServicesRetrievalError
       extends ComponentError("0018", s"Unexpected error while retrieving flattened E-Services")
 
@@ -75,57 +59,73 @@ object CatalogProcessErrors {
   final case class UpdateDraftDescriptorError(eServiceId: String)
       extends ComponentError("0020", s"Error while updating draft Descriptor for e-service identified as $eServiceId")
   final case class UpdateEServiceError(eServiceId: String)
-      extends ComponentError("0020", s"Error while updating e-service having Id $eServiceId")
+      extends ComponentError("0021", s"Error while updating e-service having Id $eServiceId")
 
   final case class DeleteDescriptorDocumentBadRequest(documentId: String, descriptorId: String, eServiceId: String)
-      extends ComponentError(
-        "0021",
-        s"Error deleting document $documentId for E-Service $eServiceId and descriptor $descriptorId"
-      )
-  final case class DeleteDescriptorDocumentNotFound(documentId: String, descriptorId: String, eServiceId: String)
       extends ComponentError(
         "0022",
         s"Error deleting document $documentId for E-Service $eServiceId and descriptor $descriptorId"
       )
-  final case class DeleteDescriptorDocumentError(documentId: String, descriptorId: String, eServiceId: String)
+  final case class DeleteDescriptorDocumentNotFound(documentId: String, descriptorId: String, eServiceId: String)
       extends ComponentError(
         "0023",
+        s"Error deleting document $documentId for E-Service $eServiceId and descriptor $descriptorId"
+      )
+  final case class DeleteDescriptorDocumentError(documentId: String, descriptorId: String, eServiceId: String)
+      extends ComponentError(
+        "0024",
         s"Error deleting document $documentId for E-Service $eServiceId and descriptor $descriptorId"
       )
 
   final case class UpdateDescriptorDocumentBadRequest(documentId: String, descriptorId: String, eServiceId: String)
       extends ComponentError(
-        "0024",
+        "0025",
         s"Error updating document $documentId for E-Service $eServiceId and descriptor $descriptorId"
       )
   final case class UpdateDescriptorDocumentNotFound(documentId: String, descriptorId: String, eServiceId: String)
       extends ComponentError(
-        "0025",
+        "0026",
         s"Error updating document $documentId for E-Service $eServiceId and descriptor $descriptorId"
       )
   final case class UpdateDescriptorDocumentError(documentId: String, descriptorId: String, eServiceId: String)
       extends ComponentError(
-        "0026",
+        "0027",
         s"Error updating document $documentId for E-Service $eServiceId and descriptor $descriptorId"
       )
 
   final case class CloneDescriptorError(descriptorId: String, eServiceId: String)
-      extends ComponentError("0027", s"Error while cloning descriptor $descriptorId for E-service $eServiceId")
+      extends ComponentError("0028", s"Error while cloning descriptor $descriptorId for E-service $eServiceId")
 
   final case class EServiceDeletionError(eServiceId: String)
-      extends ComponentError("0028", s"Error while deleting E-Service $eServiceId")
+      extends ComponentError("0029", s"Error while deleting E-Service $eServiceId")
 
   final case class ActivateDescriptorDocumentBadRequest(descriptorId: String, eServiceId: String)
-      extends ComponentError("0029", s"Error while activating descriptor $descriptorId for E-Service $eServiceId")
-  final case class ActivateDescriptorDocumentNotFound(descriptorId: String, eServiceId: String)
       extends ComponentError("0030", s"Error while activating descriptor $descriptorId for E-Service $eServiceId")
-  final case class ActivateDescriptorDocumentError(descriptorId: String, eServiceId: String)
+  final case class ActivateDescriptorDocumentNotFound(descriptorId: String, eServiceId: String)
       extends ComponentError("0031", s"Error while activating descriptor $descriptorId for E-Service $eServiceId")
+  final case class ActivateDescriptorDocumentError(descriptorId: String, eServiceId: String)
+      extends ComponentError("0032", s"Error while activating descriptor $descriptorId for E-Service $eServiceId")
 
   final case class SuspendDescriptorDocumentBadRequest(descriptorId: String, eServiceId: String)
-      extends ComponentError("0032", s"Error while suspending descriptor $descriptorId for E-Service $eServiceId")
-  final case class SuspendDescriptorDocumentNotFound(descriptorId: String, eServiceId: String)
       extends ComponentError("0033", s"Error while suspending descriptor $descriptorId for E-Service $eServiceId")
-  final case class SuspendDescriptorDocumentError(descriptorId: String, eServiceId: String)
+  final case class SuspendDescriptorDocumentNotFound(descriptorId: String, eServiceId: String)
       extends ComponentError("0034", s"Error while suspending descriptor $descriptorId for E-Service $eServiceId")
+  final case class SuspendDescriptorDocumentError(descriptorId: String, eServiceId: String)
+      extends ComponentError("0035", s"Error while suspending descriptor $descriptorId for E-Service $eServiceId")
+
+  final case class GetDescriptorDocumentBadRequest(documentId: String, descriptorId: String, eServiceId: String)
+      extends ComponentError(
+        "0036",
+        s"Error retrieving document $documentId for E-Service $eServiceId and descriptor $descriptorId"
+      )
+  final case class GetDescriptorDocumentNotFound(documentId: String, descriptorId: String, eServiceId: String)
+      extends ComponentError(
+        "0037",
+        s"Error retrieving document $documentId for E-Service $eServiceId and descriptor $descriptorId"
+      )
+  final case class GetDescriptorDocumentError(documentId: String, descriptorId: String, eServiceId: String)
+      extends ComponentError(
+        "0038",
+        s"Error retrieving document $documentId for E-Service $eServiceId and descriptor $descriptorId"
+      )
 }
