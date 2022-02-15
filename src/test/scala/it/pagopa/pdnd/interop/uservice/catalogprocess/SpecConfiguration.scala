f3e7e736e777d63d4209d7130b2cee3ae12d5c66
galales <25105748+galales@users.noreply.github.com>
2022-02-15 12:30:29 +0100

    pin-1097: Do no use application.conf in tests

--- src/test/scala/it/pagopa/pdnd/interop/uservice/catalogprocess/SpecConfiguration.scala
+++ src/test/scala/it/pagopa/pdnd/interop/uservice/catalogprocess/SpecConfiguration.scala
@@ -6,20 +6,6 @@ import com.typesafe.config.{Config, ConfigFactory}
   */
 trait SpecConfiguration {
 
-  System.setProperty("AWS_ACCESS_KEY_ID", "foo")
-  System.setProperty("AWS_SECRET_ACCESS_KEY", "bar")
-  System.setProperty("CATALOG_MANAGEMENT_URL", "http://localhost/")
-  System.setProperty("AGREEMENT_MANAGEMENT_URL", "http://localhost/")
-  System.setProperty("PARTY_MANAGEMENT_URL", "http://localhost/")
-  System.setProperty("ATTRIBUTE_REGISTRY_MANAGEMENT_URL", "http://localhost/")
-  System.setProperty("WELL_KNOWN_URL", "http://localhost/.well-known/jwks.json")
-  System.setProperty("STORAGE_TYPE", "File")
-  System.setProperty("STORAGE_CONTAINER", "localhost")
-  System.setProperty("STORAGE_ENDPOINT", "localhost")
-  System.setProperty("STORAGE_CREDENTIAL_ID", "localhost")
-  System.setProperty("STORAGE_CREDENTIAL_SECRET", "localhost")
-  System.setProperty("MAIN_AUDIENCE", "audience")
-
   val config: Config = ConfigFactory
     .parseResourcesAnySyntax("application-test")
 
