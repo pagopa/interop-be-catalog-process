d40b2e5c1b772b1ebebf9983bdafa5865b68e822
P3trur0 <ubaldopescatore@gmail.com>
2022-03-17 18:20:23 +0100

    changed catalog-mgmt dependency version

--- project/Versions.scala
+++ project/Versions.scala
@@ -28,7 +28,7 @@ object Versions {
 object PagopaVersions {
   lazy val agreementManagementVersion         = "0.1.1"
   lazy val attributeRegistryManagementVersion = "0.1.1"
-  lazy val catalogManagementVersion           = "0.1.1"
+  lazy val catalogManagementVersion           = "0.1.x-SNAPSHOT"
   lazy val commonsVersion                     = "0.1.1"
   lazy val partyManagementVersion             = "0.1.7"
   lazy val authorizationManagementVersion     = "0.1.1"
