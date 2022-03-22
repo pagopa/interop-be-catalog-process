e2a73c46485b2b76d13ce84f6d87012a2f18d20f
Antonio Gelameris <toniogela89@gmail.com>
2022-03-22 17:39:24 +0100

    Bumping dependencies versions (#72)
    * Bumping dependencies versions
    
    * Removed aws

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,29 +5,17 @@ import sbt._
 object Dependencies {
 
   private[this] object akka {
-    lazy val namespace  = "com.typesafe.akka"
-    lazy val actorTyped = namespace                       %% "akka-actor-typed"     % akkaVersion
-    lazy val stream     = namespace                       %% "akka-stream"          % akkaVersion
-    lazy val http       = namespace                       %% "akka-http"            % akkaHttpVersion
-    lazy val httpJson   = namespace                       %% "akka-http-spray-json" % akkaHttpVersion
-    lazy val httpJson4s = "de.heikoseeberger"             %% "akka-http-json4s"     % akkaHttpJson4sVersion
-    lazy val management = "com.lightbend.akka.management" %% "akka-management"      % akkaManagementVersion
+    lazy val namespace           = "com.typesafe.akka"
+    lazy val actorTyped          = namespace                       %% "akka-actor-typed"     % akkaVersion
+    lazy val stream              = namespace                       %% "akka-stream"          % akkaVersion
+    lazy val http                = namespace                       %% "akka-http"            % akkaHttpVersion
+    lazy val httpJson            = namespace                       %% "akka-http-spray-json" % akkaHttpVersion
+    lazy val httpJson4s          = "de.heikoseeberger"             %% "akka-http-json4s"     % akkaHttpJson4sVersion
+    lazy val management          = "com.lightbend.akka.management" %% "akka-management"      % akkaManagementVersion
     lazy val managementLogLevels =
       "com.lightbend.akka.management" %% "akka-management-loglevels-logback" % akkaManagementVersion
     lazy val slf4j   = namespace %% "akka-slf4j"               % akkaVersion
     lazy val testkit = namespace %% "akka-actor-testkit-typed" % akkaVersion
-  }
-
-  private[this] object awssdk {
-    lazy val namespace = "software.amazon.awssdk"
-    lazy val s3        = namespace % "s3" % awsSdkVersion
-  }
-
-  lazy val Protobuf = "protobuf"
-
-  private[this] object scalaprotobuf {
-    lazy val namespace = "com.thesamet.scalapb"
-    lazy val core      = namespace %% "scalapb-runtime" % scalapb.compiler.Version.scalapbVersion
   }
 
   private[this] object json4s {
@@ -109,9 +97,9 @@ object Dependencies {
   object Jars {
     lazy val overrides: Seq[ModuleID] =
       Seq(jackson.annotations % Compile, jackson.core % Compile, jackson.databind % Compile)
-    lazy val `server`: Seq[ModuleID] = Seq(
+    lazy val `server`: Seq[ModuleID]  = Seq(
       // For making Java 12 happy
-      "javax.annotation" % "javax.annotation-api" % "1.3.2" % "compile",
+      "javax.annotation"                       % "javax.annotation-api" % "1.3.2" % "compile",
       //
       akka.actorTyped                          % Compile,
       akka.management                          % Compile,
@@ -120,7 +108,6 @@ object Dependencies {
       akka.http                                % Compile,
       akka.httpJson                            % Compile,
       cats.core                                % Compile,
-      awssdk.s3                                % Compile,
       commons.fileUpload                       % Compile,
       mustache.compiler                        % Compile,
       logback.classic                          % Compile,
@@ -136,12 +123,11 @@ object Dependencies {
       pagopa.commons                           % Compile,
       pagopa.fileManager                       % Compile,
       pagopa.commonsJWT                        % Compile,
-      scalaprotobuf.core                       % Protobuf,
       akka.testkit                             % Test,
       scalatest.core                           % Test,
       scalamock.core                           % Test
     )
-    lazy val client: Seq[ModuleID] = Seq(
+    lazy val client: Seq[ModuleID]    = Seq(
       akka.stream     % Compile,
       akka.http       % Compile,
       akka.httpJson4s % Compile,
