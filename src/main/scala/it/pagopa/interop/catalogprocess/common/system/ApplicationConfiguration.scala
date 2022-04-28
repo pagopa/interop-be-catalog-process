39fe405ae6091c5ceaf76be03e5d7223ffd81f85
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-04-28 09:39:05 +0200

    pin-1382: Comma seperated strings for accepted audiences (#91)

--- src/main/scala/it/pagopa/interop/catalogprocess/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/catalogprocess/common/system/ApplicationConfiguration.scala
@@ -2,26 +2,25 @@ package it.pagopa.interop.catalogprocess.common.system
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-import scala.jdk.CollectionConverters.CollectionHasAsScala
-
 object ApplicationConfiguration {
-  lazy val config: Config = ConfigFactory.load()
+  val config: Config = ConfigFactory.load()
 
-  lazy val serverPort: Int = config.getInt("catalog-process.port")
+  val serverPort: Int = config.getInt("catalog-process.port")
 
-  lazy val catalogManagementUrl: String = config.getString("catalog-process.services.catalog-management")
+  val catalogManagementUrl: String = config.getString("catalog-process.services.catalog-management")
 
-  lazy val agreementManagementUrl: String = config.getString("catalog-process.services.agreement-management")
+  val agreementManagementUrl: String = config.getString("catalog-process.services.agreement-management")
 
-  lazy val authorizationManagementUrl: String = config.getString("catalog-process.services.authorization-management")
+  val authorizationManagementUrl: String = config.getString("catalog-process.services.authorization-management")
 
-  lazy val attributeRegistryManagementUrl: String =
+  val attributeRegistryManagementUrl: String =
     config.getString("catalog-process.services.attribute-registry-management")
 
-  lazy val partyManagementUrl: String = config.getString("catalog-process.services.party-management")
+  val partyManagementUrl: String = config.getString("catalog-process.services.party-management")
 
-  lazy val jwtAudience: Set[String] = config.getStringList("catalog-process.jwt.audience").asScala.toSet
+  val jwtAudience: Set[String] = config.getString("catalog-process.jwt.audience").split(",").toSet.filter(_.nonEmpty)
 
-  lazy val storageContainer: String = config.getString("catalog-process.storage.container")
+  val storageContainer: String = config.getString("catalog-process.storage.container")
 
+  require(jwtAudience.nonEmpty, "Audience cannot be empty")
 }
