a2cd6f8bd61c31a3f76dfff52559c28476a4c5a3
Antonio Gelameris <toniogela89@gmail.com>
2022-07-01 17:05:43 +0200

    Updated Dependencies (#115)
    * Update sbt-tpolecat to 0.2.3 in 0.1.x
    
    * Update scalatest to 3.2.12 in 0.1.x
    
    * Update scalafmt-core to 3.5.8 in 0.1.x
    
    * Update kamon-bundle, kamon-prometheus to 2.5.4 in 0.1.x
    
    Co-authored-by: Scala Steward <scala-steward@pagopa.it>

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   lazy val jacksonVersion              = "2.11.4"
   lazy val jdbcPersistenceVersion      = "5.0.1"
   lazy val json4sVersion               = "4.0.4"
-  lazy val kamonVersion                = "2.5.1"
+  lazy val kamonVersion                = "2.5.4"
   lazy val logbackVersion              = "1.2.11"
   lazy val mustacheVersion             = "0.9.10"
   lazy val openapi4jVersion            = "1.0.7"
@@ -19,7 +19,7 @@ object Versions {
   lazy val s3Persistence               = "1.0.27"
   lazy val scalaMockVersion            = "5.2.0"
   lazy val scalapbVersion              = "0.11.1"
-  lazy val scalatestVersion            = "3.2.11"
+  lazy val scalatestVersion            = "3.2.12"
   lazy val slickVersion                = "3.3.3"
   lazy val swaggerValidatorVersion     = "2.23.0"
 }
