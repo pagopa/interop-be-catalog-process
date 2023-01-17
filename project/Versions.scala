08953a6a7c92b0e80c5144aea53774ad529b0970
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-01-17 15:39:38 +0100

    bump versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -24,10 +24,10 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val agreementManagementVersion         = "1.0.x-SNAPSHOT"
-  lazy val attributeRegistryManagementVersion = "1.0.x-SNAPSHOT"
-  lazy val catalogManagementVersion           = "1.0.x-SNAPSHOT"
-  lazy val commonsVersion                     = "1.0.x-SNAPSHOT"
-  lazy val tenantManagementVersion            = "1.0.x-SNAPSHOT"
-  lazy val authorizationManagementVersion     = "1.0.x-SNAPSHOT"
+  lazy val agreementManagementVersion         = "1.0.5"
+  lazy val attributeRegistryManagementVersion = "1.0.7"
+  lazy val catalogManagementVersion           = "1.0.9"
+  lazy val commonsVersion                     = "1.0.12"
+  lazy val tenantManagementVersion            = "1.0.3"
+  lazy val authorizationManagementVersion     = "1.0.8"
 }
