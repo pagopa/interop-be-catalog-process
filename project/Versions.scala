c5895112e78d8169454aeec46341cd701fbd7ba2
Antonio Gelameris <toniogela89@gmail.com>
2023-06-14 11:50:25 +0200

    Reverted catalog management dep

--- project/Versions.scala
+++ project/Versions.scala
@@ -26,7 +26,7 @@ object Versions {
 object PagopaVersions {
   lazy val agreementManagementVersion         = "1.0.x-SNAPSHOT"
   lazy val attributeRegistryManagementVersion = "1.0.x-SNAPSHOT"
-  lazy val catalogManagementVersion           = "PIN-3377-SNAPSHOT"
+  lazy val catalogManagementVersion           = "1.0.x-SNAPSHOT"
   lazy val commonsVersion                     = "1.0.x-SNAPSHOT"
   lazy val tenantManagementVersion            = "1.0.x-SNAPSHOT"
   lazy val authorizationManagementVersion     = "1.0.x-SNAPSHOT"
