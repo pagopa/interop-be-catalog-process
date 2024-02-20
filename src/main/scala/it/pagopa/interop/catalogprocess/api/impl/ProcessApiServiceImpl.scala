ce1cb34c4476d59357fa230d14bd7848bd74bfaf
nttdata-rtorsoli <riccardo.torsoli@emeal.nttdata.com>
2024-02-20 13:30:44 +0100

    PIN-4571 Aliogned to 1.0.,x

--- src/main/scala/it/pagopa/interop/catalogprocess/api/impl/ProcessApiServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/catalogprocess/api/impl/ProcessApiServiceImpl.scala
@@ -120,6 +120,10 @@ final case class ProcessApiServiceImpl(
       .find(_.id.toString == descriptorId)
       .toRight(EServiceDescriptorNotFound(eService.id.toString, descriptorId))
 
+private def getLatestDescriptor(eService: CatalogItem): Option[CatalogDescriptor] = eService.descriptors
+    .sortBy(_.version.toInt)(Ordering.Int.reverse)
+    .headOption
+
   override def getEServices(
     name: Option[String],
     eServicesIds: String,
@@ -423,6 +427,22 @@ final case class ProcessApiServiceImpl(
       _                         <- assertRequesterAllowed(catalogItem.producerId)(organizationId)
       _                         <- hasNotDraftDescriptor(catalogItem).toFuture
       _                         <- eServiceDescriptorSeed.attributes.certified.traverse(
+      _                         <- checkDailyCalls(
+        eServiceDescriptorSeed.dailyCallsPerConsumer,
+        eServiceDescriptorSeed.dailyCallsTotal
+      ).toFuture
+      _                         <- getLatestDescriptor(catalogItem).fold(Future.unit)(descriptor =>
+        checkIfDailyCallsIsGreaterThanExisting(
+          eServiceDescriptorSeed.dailyCallsPerConsumer,
+          descriptor.dailyCallsPerConsumer
+        ).toFuture
+      )
+      _                         <- getLatestDescriptor(catalogItem).fold(Future.unit)(descriptor =>
+        checkIfDailyCallsIsGreaterThanExisting(
+          eServiceDescriptorSeed.dailyCallsTotal,
+          descriptor.dailyCallsTotal
+        ).toFuture
+      )  
         _.traverse(attr => attributeRegistryManagementService.getAttributeById(attr.id))
       )
       _                         <- eServiceDescriptorSeed.attributes.declared.traverse(
@@ -462,6 +482,18 @@ final case class ProcessApiServiceImpl(
       _               <- assertRequesterAllowed(catalogItem.producerId)(organizationId)
       descriptor      <- assertDescriptorExists(catalogItem, descriptorUuid)
       _               <- isDraftDescriptor(descriptor)
+       _               <- checkDailyCalls(
+        updateEServiceDescriptorSeed.dailyCallsPerConsumer,
+        updateEServiceDescriptorSeed.dailyCallsTotal
+      ).toFuture
+      _               <- checkIfDailyCallsIsGreaterThanExisting(
+        updateEServiceDescriptorSeed.dailyCallsPerConsumer,
+        descriptor.dailyCallsPerConsumer
+      ).toFuture
+      _               <- checkIfDailyCallsIsGreaterThanExisting(
+        updateEServiceDescriptorSeed.dailyCallsTotal,
+        descriptor.dailyCallsTotal
+      ).toFuture
       updatedEService <- catalogManagementService.updateDescriptor(
         eServiceId,
         descriptorId,
@@ -484,13 +516,19 @@ final case class ProcessApiServiceImpl(
       logger.info(operationLabel)
 
       val result: Future[EService] = for {
-        organizationId  <- getOrganizationIdFutureUUID(contexts)
-        eServiceUuid    <- eServiceId.toFutureUUID
-        descriptorUuid  <- descriptorId.toFutureUUID
-        catalogItem     <- catalogManagementService.getEServiceById(eServiceUuid)
-        _               <- assertRequesterAllowed(catalogItem.producerId)(organizationId)
-        descriptor      <- assertDescriptorExists(catalogItem, descriptorUuid)
-        _               <- descriptorCanBeUpdated(descriptor)
+        organizationId <- getOrganizationIdFutureUUID(contexts)
+        eServiceUuid   <- eServiceId.toFutureUUID
+        descriptorUuid <- descriptorId.toFutureUUID
+        catalogItem    <- catalogManagementService.getEServiceById(eServiceUuid)
+        _              <- assertRequesterAllowed(catalogItem.producerId)(organizationId)
+        descriptor     <- assertDescriptorExists(catalogItem, descriptorUuid)
+        _              <- descriptorCanBeUpdated(descriptor)
+        _              <- checkDailyCalls(seed.dailyCallsPerConsumer, seed.dailyCallsTotal).toFuture
+        _              <- checkIfDailyCallsIsGreaterThanExisting(
+          seed.dailyCallsPerConsumer,
+          descriptor.dailyCallsPerConsumer
+        ).toFuture
+        _ <- checkIfDailyCallsIsGreaterThanExisting(seed.dailyCallsTotal, descriptor.dailyCallsTotal).toFuture
         updatedEService <- catalogManagementService.updateDescriptor(
           eServiceId,
           descriptorId,
